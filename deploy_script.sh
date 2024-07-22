b589638a183789196808c39a0c4e83ff63be2977
Rudrank1 <rudrank2004@gmail.com>
2024-07-23 03:47:03 +0530

    Reverted

--- deploy_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Log in to Azure
-az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-
-# Configure the Web App to use the Docker image
-az webapp config container set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --docker-custom-image-name $DOCKER_IMAGE
